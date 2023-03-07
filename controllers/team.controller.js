ec25df9dfb0463bc6a67620c8fd11b41b7f1f8fe
Thibaut Monin <device.thibaut.monin@gmail.com>
2023-03-07 21:07:23 +0100

    Updating team controller (updateTeam, deleteTeam)

--- controllers/team.controller.js
+++ controllers/team.controller.js
@@ -16,3 +16,29 @@ module.exports.createTeam = async (req, res) => {
 
   res.status(200).json(team)
 }
+
+module.exports.updateTeam = async (req, res) => {
+  const team = await TeamModel.findById(req.params.id)
+
+  if (!team) {
+    res.status(400).json({ error: "This team doesn't exist." })
+  }
+
+  const updatedTeam = await TeamModel.findByIdAndUpdate(team, req.body, { new: true })
+
+  res.status(200).json(updatedTeam)
+
+}
+
+module.exports.deleteTeam = async (req, res) => {
+  const team = await TeamModel.findById(req.params.id)
+
+  if (!team) {
+    res.status(400).json({ error: "This team doesn't exist." })
+  }
+
+  await TeamModel.findByIdAndRemove(team)
+
+  res.status(200).json(`Team with id ${req.params.id} has been removed.`)
+
+}
