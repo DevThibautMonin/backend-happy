27ea7f6b6f780057bdeee2cfb39867527c8f861b
Thibaut Monin <device.thibaut.monin@gmail.com>
2023-03-07 21:07:11 +0100

    Updating team routes (put, delete)

--- routes/team/team.routes.js
+++ routes/team/team.routes.js
@@ -1,8 +1,10 @@
 const express = require('express')
-const { createTeam, getTeams } = require('../../controllers/team.controller')
+const { createTeam, getTeams, updateTeam, deleteTeam } = require('../../controllers/team.controller')
 const teamRouter = express.Router()
 
 teamRouter.post('/', createTeam)
 teamRouter.get('/', getTeams)
+teamRouter.put('/:id', updateTeam)
+teamRouter.delete('/:id', deleteTeam)
 
 module.exports = teamRouter
