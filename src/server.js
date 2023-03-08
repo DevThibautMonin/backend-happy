205e52a53be02a0d7bd8c734b438a6b6776fc4e7
Thibaut Monin <device.thibaut.monin@gmail.com>
2023-03-08 13:43:26 +0100

    Updating server

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const connectDB = require('./config/db')
 const app = express()
 const dotenv = require('dotenv').config()
 const teamRouter = require('./routes/team/team.routes')
+const projectRouter = require('./routes/project/project.routes')
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
@@ -10,6 +11,7 @@ app.use(express.urlencoded({ extended: false }))
 connectDB()
 
 app.use(`/api/v${process.env.API_VERSION}/team`, teamRouter)
+app.use(`/api/v${process.env.API_VERSION}/project`, projectRouter)
 
 app.listen(process.env.PORT, () => {
   console.log(`Server started on port ${process.env.PORT}`)
