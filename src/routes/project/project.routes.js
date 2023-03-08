39959c1c6a56d56f58c92ab5c6c18e01c5bcc558
Thibaut Monin <device.thibaut.monin@gmail.com>
2023-03-08 13:43:49 +0100

    Add: Project routes

---
+++ src/routes/project/project.routes.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const projectRouter = express.Router()
+const { createProject, getProjects, updateProject, deleteProject } = require('../../controllers/project.controller')
+
+projectRouter.post('/', createProject)
+projectRouter.get('/', getProjects)
+projectRouter.put('/:id', updateProject)
+projectRouter.delete('/:id', deleteProject)
+
+module.exports = projectRouter
